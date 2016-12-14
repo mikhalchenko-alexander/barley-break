5ef508d249ae5f908048b77b78586820e59eb546
Alexander Mikhalchenko <aam@anadeainc.com>
2016-12-14 08:38:08 +0200

    Initial commit.

---
+++ Assets/Scripts/ClickDelete.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickDelete : MonoBehaviour {
+
+    public GameObject cube;
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+            {
+                if (hit.collider.gameObject == cube)
+                {
+                    Destroy(gameObject);
+                }
+            }           
+        }
+	}
+}
