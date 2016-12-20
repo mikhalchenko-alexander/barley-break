87c89026401d51be43dad34f3fd4f92bb377c118
aam <aam@anahoret.com>
2016-12-20 18:55:35 +0200

    Add tile animation.

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -11,6 +11,10 @@ public class Game : MonoBehaviour {
     private GameObject youWin;
     private GameObject tilesContainer;
 
+	private GameObject movingTile;
+	private Vector3 tileTarget;
+	private bool moving = false;
+
     // Use this for initialization
     void Start () {
 		tiles = GameObject.FindGameObjectsWithTag("tile");
@@ -31,20 +35,25 @@ public class Game : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	    if (!Input.GetMouseButtonDown(0)) return;
-	    RaycastHit hit;
-	    if (!Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit)) return;
-		GameObject obj = hit.collider.gameObject.transform.parent.gameObject;
-		if (obj.tag == "tile")
-		{
-			GameObject tile = obj;
-            bool moved = move(tile);
+		if (moving) {
+			bool moved = move (movingTile, false);
+			if (moved && isWin ()) {
+				tilesContainer.SetActive (false);
+				youWin.SetActive (true);
+			}
+		} else if (Input.GetMouseButtonDown(0)) {
+			RaycastHit hit;
+			if (!Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit) && !moving) return;
+			GameObject obj = hit.collider.gameObject.transform.parent.gameObject;
+			if (obj.tag == "tile" && !moving) {
+				GameObject tile = obj;
+				bool moved = move (tile, false);
 
-            if (moved && isWin())
-            {
-                tilesContainer.SetActive(false);
-                youWin.SetActive(true);
-            }
+				if (moved && isWin ()) {
+					tilesContainer.SetActive (false);
+					youWin.SetActive (true);
+				}
+			}
 		}
 	}
 
@@ -66,7 +75,8 @@ public class Game : MonoBehaviour {
     {
         List<KeyValuePair<int, int>> validMoves = new List<KeyValuePair<int, int>>();
         System.Random rnd = new System.Random();
-        for (int i = 0; i < 200; i++)
+		int fact = 0;
+		for (int i = 0; i < 200; i++)
         {
             int zeroIndex = -1;
             for (int j = 0; j < 16; j++)
@@ -89,7 +99,7 @@ public class Game : MonoBehaviour {
             int idx = validMove.Key * 4 + validMove.Value;
             int tileNumber = field[idx];
             GameObject tile = tiles[tileNumber - 1];
-            move(tile);
+            move(tile, true);
 
             validMoves.Clear();
         }
@@ -102,68 +112,94 @@ public class Game : MonoBehaviour {
         Application.Quit();
     }
 
-    private bool move(GameObject tile)
+	private bool move(GameObject tile, bool immediate)
     {
-        int tileNum = tileNumber(tile.name);
-
-        int tileIndex = -1;
-        int zeroIndex = -1;
-        for (int i = 0; i < 16; i++)
-        {
-            if (field[i] == tileNum)
-                tileIndex = i;
-            if (field[i] == 0)
-                zeroIndex = i;
-            if (tileIndex >= 0 && zeroIndex >= 0)
-                break;
-        }
-
-        int tileRow = tileIndex / 4;
-        int tileCol = tileIndex % 4;
-
-        int zeroRow = zeroIndex / 4;
-        int zeroCol = zeroIndex % 4;
-
+		int tileNum = tileNumber(tile.name);
         bool moved = false;
 
-        if (tileRow == zeroRow)
-        {
-            if (tileCol == zeroCol + 1)
-            {
-                Vector3 p = tiles[tileNum - 1].transform.position;
-                tiles[tileNum - 1].transform.position = new Vector3(p.x - tileSize, p.y, p.z);
-                field[zeroIndex] = field[tileIndex];
-                field[tileIndex] = 0;
-                moved = true;
-            }
-            else if (tileCol == zeroCol - 1)
-            {
-                Vector3 p = tiles[tileNum - 1].transform.position;
-                tiles[tileNum - 1].transform.position = new Vector3(p.x + tileSize, p.y, p.z);
-                field[zeroIndex] = field[tileIndex];
-                field[tileIndex] = 0;
-                moved = true;
-            }
-        }
-        else if (tileCol == zeroCol)
-        {
-            if (tileRow == zeroRow + 1)
-            {
-                Vector3 p = tiles[tileNum - 1].transform.position;
-                tiles[tileNum - 1].transform.position = new Vector3(p.x, p.y + tileSize, p.z);
-                field[zeroIndex] = field[tileIndex];
-                field[tileIndex] = 0;
-                moved = true;
-            }
-            else if (tileRow == zeroRow - 1)
-            {
-                Vector3 p = tiles[tileNum - 1].transform.position;
-                tiles[tileNum - 1].transform.position = new Vector3(p.x, p.y - tileSize, p.z);
-                field[zeroIndex] = field[tileIndex];
-                field[tileIndex] = 0;
-                moved = true;
-            }
-        }
+		if (!moving)
+		{
+			int tileIndex = -1;
+			int zeroIndex = -1;
+			for (int i = 0; i < 16; i++)
+			{
+				if (field[i] == tileNum)
+					tileIndex = i;
+				if (field[i] == 0)
+					zeroIndex = i;
+				if (tileIndex >= 0 && zeroIndex >= 0)
+					break;
+			}
+
+			int tileRow = tileIndex / 4;
+			int tileCol = tileIndex % 4;
+
+			int zeroRow = zeroIndex / 4;
+			int zeroCol = zeroIndex % 4;
+
+			if (tileRow == zeroRow)
+			{
+				if (tileCol == zeroCol + 1)
+				{
+					movingTile = tile;
+					Vector3 p = movingTile.transform.position;
+					tileTarget = new Vector3(p.x - tileSize, p.y, p.z);
+					moving = true;
+					field[zeroIndex] = field[tileIndex];
+					field[tileIndex] = 0;
+				}
+				else if (tileCol == zeroCol - 1)
+				{
+					movingTile = tile;
+					Vector3 p = movingTile.transform.position;
+					tileTarget = new Vector3(p.x + tileSize, p.y, p.z);
+					moving = true;
+					field[zeroIndex] = field[tileIndex];
+					field[tileIndex] = 0;
+				}
+			}
+			else if (tileCol == zeroCol)
+			{
+				if (tileRow == zeroRow + 1)
+				{
+					movingTile = tile;
+					Vector3 p = movingTile.transform.position;
+					tileTarget = new Vector3(p.x, p.y + tileSize, p.z);
+					moving = true;
+					field[zeroIndex] = field[tileIndex];
+					field[tileIndex] = 0;
+				}
+				else if (tileRow == zeroRow - 1)
+				{
+					movingTile = tile;
+					Vector3 p = movingTile.transform.position;
+					tileTarget = new Vector3(p.x, p.y - tileSize, p.z);
+					moving = true;
+					field[zeroIndex] = field[tileIndex];
+					field[tileIndex] = 0;
+				}
+			}
+		}
+
+		if (moving)
+		{
+			if (immediate) {
+				movingTile.transform.position = tileTarget;
+				moving = false;
+				moved = true;
+			} else {
+				Vector3 currentTilePos = movingTile.transform.position;
+
+				if (Vector3.Distance (tileTarget, currentTilePos) > 0.00001) {
+					movingTile.transform.position = Vector3.MoveTowards (currentTilePos, tileTarget, Time.deltaTime * 10);
+				} else {
+					movingTile.transform.position = tileTarget;
+					moving = false;
+					moved = true;
+				}
+			}
+		}
+
         return moved;
     }
 
